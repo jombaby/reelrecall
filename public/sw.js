136795f723d655a64c729ded7c484fda32f8ae57
Jom <jom@Joms-MacBook-Pro.local>
2026-09-03 11:25:29 -0400

    Fix direct Facebook and Instagram reel AI analysis

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 <<<<<<< ours
 const CACHE="reelreplay-icons-20260903",SHELL=["/manifest.webmanifest","/icons/reelrecall.svg","/icons/reelrecall-maskable.svg"];
 =======
-const CACHE="reelrecall-pwa-v8",SHELL=["/manifest.webmanifest","/icons/reelrecall.svg","/icons/reelrecall-maskable.svg"];
+const CACHE="reelrecall-pwa-v9",SHELL=["/manifest.webmanifest","/icons/reelrecall.svg","/icons/reelrecall-maskable.svg"];
 >>>>>>> theirs
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
